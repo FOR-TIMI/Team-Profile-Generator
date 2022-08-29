b61938f22b29278bcada5fe6de70d21c803e9c95
unknown <timiplayskeys@gmail.com>
2022-08-29 15:18:18 -0400

    creating a shorter syntax

--- index.js
+++ index.js
@@ -4,21 +4,35 @@ const Manager = require('./lib/Manager');
 
 
 function init(){
-    let manager = new Manager();
+   
     const app = {}
     app.setManagerData = function(){
-        inquirer
-        .prompt(manager.questions())
-        .then(answers => {
-              const {id, name, email,officeNumber} = answers
-              manager.id = id;
-              manager.name = name;
-              manager.email = email;
-              manager.officeNumber = officeNumber;
-              manager.role = manager.getRole();
+        const questions = new Manager('chris',1234124,'chris@ym.com',21412)
+        var props = Object.getOwnPropertyNames(questions);
+        // const props = Object.getOwnPropertyNames(role);
+
+        //loop through arguments
+    
+        //set property to the argument
+        for(let property of props){
+            Manager[property] = property
+        }
+
+        console.log(questions)
+        
+
+        // inquirer
+        // .prompt(questions())
+        // .then(answers => {
+        //       const {id, name, email,officeNumber} = answers
+        //       manager.id = id;
+        //       manager.name = name;
+        //       manager.email = email;
+        //       manager.officeNumber = officeNumber;
+        //       manager.role = manager.getRole();
  
-              manager.addEmployee();
-            })
+        //       manager.addEmployee();
+        //     })
     }
     app.setManagerData();
 
