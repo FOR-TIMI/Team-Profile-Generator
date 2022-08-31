8d8c0f72557dc5b28d9c80daaf34afab403ea7cc
unknown <timiplayskeys@gmail.com>
2022-08-31 18:32:15 -0400

    added less for styling

--- src/fileData.js
+++ src/fileData.js
@@ -1,14 +1,35 @@
 const fs = require('fs');
 const path = require('path');
+let filepath = path.join(__dirname,'../dist/teams.html');
 
 
 function setFileHtml(data){
-    return `<h1>I love you sunny</h1>`
+
+    if(fs.existsSync(filepath)){
+        return `<h1>I love you sunny</h1>`
+    }
+
+    //To return HTML boilerplate
+    
+    return `<!DOCTYPE html>
+    <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team</title>
+    </head>
+    <body>
+       
+        
+    </body>
+    </html>`
+    
 }
 
        
 function appendToFile(data){
-  fs.appendFileSync(path.join(__dirname,'../dist/teams.html'),String(data),(err) =>{
+  fs.appendFileSync(filepath,data,(err) =>{
     if(err) throw err;
   })
 };  
