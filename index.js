b631302b9f9910c825a37d86a90de2b033f15cce
unknown <timiplayskeys@gmail.com>
2022-09-01 22:09:14 -0400

    parsed objects to json file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
+const {saveData} = require('./src/fileData')
+
+
 
 
 
 function init(){
-    const managerQuestions = new Manager().getQuestions();
+    const managerQuestions = new Manager().questions;
     const app = {}
     app.setManagerData = function(){
         inquirer
@@ -15,8 +18,12 @@ function init(){
             //create a new manager Instance
             const manager = new Manager(name,id,email,officeNumber)
 
+            // employeeArr.push(manager);
+
             //To create a new employee card for the manager
-            manager.createCard(manager);
+            // manager.createCard(manager);
+
+            saveData({data:{manager}})
 
             //To add a new employee
             manager.addEmployee()
--- src/fileData.js
+++ src/fileData.js
@@ -3,38 +3,53 @@ const path = require('path');
 let filepath = path.join(__dirname,'../dist/teams.html');
 
 
-function setFileHtml(data){
+// function setFileHtml(data){
 
-    if(fs.existsSync(filepath)){
-        return `<h1>I love you sunny</h1>`
-    }
+//     if(fs.existsSync(filepath)){
+//         return `<h1>I love you sunny</h1>`
+//     }
 
-    //To return HTML boilerplate
+//     //To return HTML boilerplate
     
-    return `<!DOCTYPE html>
-    <html lang="en">
-   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team</title>
-    </head>
-    <body>
+//     return `<!DOCTYPE html>
+//     <html lang="en">
+//    <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title>Team</title>
+//     </head>
+//     <body>
        
         
-    </body>
-    </html>`
+//     </body>
+//     </html>`
     
-}
+// }
 
        
 function appendToFile(data){
   fs.appendFileSync(filepath,data,(err) =>{
     if(err) throw err;
   })
-};  
+}; 
+
+
+const jsonfilepath = path.join(__dirname,'../dist/teams.json')
+
+function setJsonData(data){
+  return JSON.stringify(data)
+}
+
+function saveData(data){
+  const dataText = setJsonData(data) 
+ fs.appendFileSync(jsonfilepath,dataText,err =>{
+  throw new Error('unable to save data to json file',err)
+ })
+}
 
 module.exports = {
-    setFileHtml,
-    appendToFile
+ 
+    appendToFile,
+    saveData
 }
