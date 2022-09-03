ce11d6ff0631e4b0dcfa269bcf25c411de0b17e8
unknown <timiplayskeys@gmail.com>
2022-09-03 02:50:55 -0400

    added some more tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,22 +1,51 @@
 const Employee = require('../lib/Employee')
 
 
+const tom = new Employee('tom',11332567,'tom@gmail.com');
 
 
-test('checks if the name exists ', () => {
-  const tess = new Employee('tess',114256,'tess@gmail.com');
-    expect(tess.getName()).toBe('tess');
-    expect(tess.getId()).toBe(114256);
-    expect(tess.getEmail()).toBe('tess@gmail.com');
-    expect(tess.getRole()).toBe('Employee');
+
+ test('checks if the name exists ', () => {
+    expect(tom.getName()).toBe('tom');
+
   });
 
-  test('checks if the name exists ', () => {
-    const tom = new Employee('tom',11332567,'tom@gmail.com');
-      expect(tom.getName()).toBe('tom');
-      expect(tom.getId()).toBe(11332567);
-      expect(tom.getEmail()).toBe('tom@gmail.com');
-      expect(tom.getRole()).toBe('Employee');
+  test('checks if role is employee',() =>{
+    expect(tom.getRole()).toBe('Employee');
+  })
+
+  
+  test('checks if the id is accurate ', () => {
+    expect(tom.getId()).toBe(11332567)
     });
+
+  test('checks if the email is accurate ', () => {
+    expect(tom.getEmail()).toBe('tom@gmail.com');
+
+    });
+
+  test('should get the questions for all employees', () => {
+    const employeeQuestions = [
+      {
+          type : 'input',
+          name : 'name',
+          message: `Enter the team Employee's name`
+      },
+      {
+          type : 'input',
+          name : 'id',
+          message: 'Enter a your employee ID'
+  
+      },
+      {
+          type : 'input',
+          name : 'email',
+          message: 'Enter you email address'
+  
+      }  
+  ]
+    expect(tom.getQuestions()).toEqual(employeeQuestions)
+  })
+
 
 
