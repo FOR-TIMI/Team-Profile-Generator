cf81bba17a3ee81659abb1c20a04e5b28bb65bdf
unknown <timiplayskeys@gmail.com>
2022-09-03 05:48:04 -0400

    added automatic file generation to helper file

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs');
 const path = require('path');
-let filepath = path.join(__dirname,'../dist/teams.html');
+// let filepath = path.join(__dirname,'../dist/teams.html');
 
 
 function init(){
@@ -32,18 +32,67 @@ function init(){
      
     }
 
-    app.createHtmlFile = function(data){
-        fs.writeFile(filepath,data,(err) =>{
-            if(err) throw err;
-            console.log('Team html file generated')
-          }).then(() => {
-            
-          })
+    app.createFiles = function(data){
+         this.createHTMLfile();
+         this.createAssetsFolder();
     }
+
+
+    app.createAssetsFolder = function(data){
+        return fs.mkdir(path.join(__dirname, '../dist/assets'), (err) => {
+            if(err) throw new Error('error',err);
+        
+             this.makeJsFolder();
+             this.makeCssFolder();
+    })
+
+    }
+
+    //To create JS folder
+    app.makeJsFolder = function(){
+        fs.mkdir(path.join(__dirname, '../dist/assets/js'), (err) => {
+            if(err) throw new Error('error',err);
+            this.createJsFile();
+        })
+    
+    }
+
+    //To create the css folder
+    app.makeCssFolder = function(){
+        fs.mkdir(path.join(__dirname, '../dist/assets/css'), (err) => {
+            if(err) throw new Error('error',err);
+             this.makeCssFile()
+            })
+    }
+   
+    //To create the index.html file and set inner HTML
+    app.createHTMLfile = function(data){
+        return fs.writeFile(path.join(__dirname, '../dist/app.html'),'i love you', err =>{
+                 if(err) throw new Error('error',err)
+        })
+    }
+
+    //To create the javascript file
+    app.createJsFile = function(){
+        fs.writeFile(path.join(__dirname, '../dist/assets/js/index.js'), 'tj', err =>{
+            if(err) throw new Error('error',err); 
+        })
+    }
+
+    //To create css file
+    app.makeCssFile = function(){
+        fs.writeFile(path.join(__dirname, '../dist/assets/css/styles.css'), 'tj', err =>{
+            if(err) throw new Error('error',err); 
+        })
+    
+    }
+
+    app.createFiles();
 
 
 }
 
+init();
 
 
 
